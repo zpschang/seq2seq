a631b5326d3b48129f59097eef8cc7ab12384844
张蒲石 <pushi@zhangpushis-MBP.modouwifi.com>
2017-05-07 01:30:59 +0800

    runnable

--- model.py
+++ model.py
@@ -3,9 +3,9 @@ import numpy as np
 from tensorflow.contrib.lookup import HashTable, KeyValueTensorInitializer
 from tensorflow.contrib.rnn import BasicLSTMCell, MultiRNNCell
 PAD_ID = 0
-UNK_ID = 1
-GO_ID = 2
-EOS_ID = 3
+UNK_ID = 5
+GO_ID = 0
+EOS_ID = 0
 _START_VOCAB = ['_PAD', '_UNK', '_GO', '_EOS']
 
 def weight(x, y):
@@ -19,48 +19,54 @@ def bias(y):
 def cross_entropy(x, y):
     return tf.reduce_mean(-tf.reduce_sum(x * tf.log(tf.clip_by_value(y, 1e-10, 1.0)), 1))
 
-class Model:
+class seq2seq_model:
     def __init__(self, vocab, embed, lstm_size, layer_num):
         # input: name->tensor
         self.learning_rate = 0.5
         self.input = {}
         self.input['post'] = tf.placeholder(tf.string, shape=(None, None)) # batch*len
-        self.input['post_len'] = tf.placeholder(tf.int32, shape=(None)) # batch
+        self.input['post_len'] = tf.placeholder(tf.int64, shape=(None)) # batch
         self.input['resp'] = tf.placeholder(tf.string, shape=(None, None)) # batch*len
-        self.input['resp_len'] = tf.placeholder(tf.int32, shape=(None)) # batch
+        self.input['resp_len'] = tf.placeholder(tf.int64, shape=(None)) # batch
 
         # symbol->index
         self.symbols = tf.Variable(vocab, trainable=False, name='symbols') # voc_size
-        self.sym2index = HashTable(KeyValueTensorInitializer(self.symbols, 
-            tf.Variable(np.array(range(len(vocab)), dtype=tf.int32), trainable=False))
-            , -1)
-        self.index2sym = HashTable(KeyValueTensorInitializer(tf.Variable(np.array(range(len(vocab)), dtype=tf.int32), trainable=False), self.symbols), -1)
+        self.sym2index = HashTable(
+            KeyValueTensorInitializer(self.symbols, 
+            tf.Variable(np.array(range(len(vocab)), dtype=np.int64), trainable=False))
+                , default_value=UNK_ID)
+        self.index2sym = HashTable(KeyValueTensorInitializer(tf.Variable(np.array(range(len(vocab))), trainable=False, dtype=tf.int64), self.symbols), '')
+        
+        self.embed = tf.Variable(embed)
         
         batch_size = tf.shape(self.input['post'])[0]
         max_len = tf.shape(self.input['post'])[1]
 
         post = self.sym2index.lookup(self.input['post']) # batch*len
         resp = self.sym2index.lookup(self.input['resp']) # batch*len
-        resp_shift = tf.concat([tf.ones([batch_size, 1], dtype=tf.int32) * GO_ID,
+        resp_shift = tf.concat([tf.ones([batch_size, 1], dtype=tf.int64) * GO_ID,
                             tf.split(resp, [max_len-1, 1], axis=1)[0]], axis=1) # batch*len
         std_output = tf.one_hot(resp, len(vocab))
-
-        post_embed = tf.nn.embedding_lookup(embed, post) # batch*len*embed_size
-        resp_embed = tf.nn.embedding_lookup(embed, resp_shift) # batch*len*embed_size
+        print post.get_shape()
+        post_embed = tf.nn.embedding_lookup(self.embed, post) # batch*len*embed_size
+        resp_embed = tf.nn.embedding_lookup(self.embed, resp_shift) # batch*len*embed_size
 
         # encoder
-        cell_encoder = MultiRNNCell([BasicLSTMCell(lstm_size) for _ in range(layer_num)])
-        output, state = tf.nn.dynamic_rnn(cell_encoder, post_embed, sequence_length=self.input['post_len'])
+        with tf.variable_scope('encoder'):
+            cell_encoder = MultiRNNCell([BasicLSTMCell(lstm_size) for _ in range(layer_num)])
+            output, state = tf.nn.dynamic_rnn(cell_encoder, post_embed, sequence_length=self.input['post_len'], dtype=tf.float32)
         
-        cell_decoder = MultiRNNCell([BasicLSTMCell(lstm_size) for _ in range(layer_num)])
-        decoder_output, _ = tf.nn.dynamic_rnn(cell_decoder, resp_embed, sequence_length=self.input['resp_len'], initial_state=state)
-        # resp_output: batch*len*lstm_size
+        # decoder
+        with tf.variable_scope('decoder'):
+            cell_decoder = MultiRNNCell([BasicLSTMCell(lstm_size) for _ in range(layer_num)])
+            decoder_output, _ = tf.nn.dynamic_rnn(cell_decoder, resp_embed, sequence_length=self.input['resp_len'], initial_state=state, dtype=tf.float32)
+        # decoder_output: batch*len*lstm_size
 
         W = weight(lstm_size, len(vocab))
         B = bias(len(vocab))
-        poss = tf.nn.softmax(tf.matmul(decoder_output, W) + B) # batch*len*voc_size
+        poss = tf.nn.softmax(tf.tensordot(decoder_output, W, [[2], [0]]) + B) # batch*len*voc_size
         loss = cross_entropy(std_output, poss) # []
-        int_output = tf.argmax(poss, axis=2) # batch*len
+        int_output = tf.cast(tf.argmax(poss, axis=2), tf.int64) # batch*len
         is_equal = tf.equal(int_output, resp) # batch*len
         acc = tf.reduce_mean(tf.reduce_mean(tf.cast(is_equal, tf.float32), axis=1)) # []
         str_output = self.index2sym.lookup(int_output) # batch*len
@@ -80,15 +86,20 @@ class Model:
             print('%s: %s' % (param.name, param.get_shape()))
     
     def step(self, sess, feed_input):
-        out_name = [name for name, value in self.output]
-        out_tensor = [value for name, value in self.output]
+        out_name = [name for name in self.output]
+        out_tensor = [self.output[name] for name in self.output]
+        feed_dict = {self.input[name]:feed_input[name] for name in self.input}
         print out_name, '=',
-        res = sess.run(out_tensor + [self.train], feed_dict={self.input[name]:feed_input[name] for name in self.input})
+        res = sess.run(out_tensor + [self.train], feed_dict=feed_dict)
         print res
     
     def inference(self, sess, feed_input):
-        out_name = [name for name, value in self.output]
-        out_tensor = [value for name, value in self.output]
+        out_name = [name for name in self.output]
+        out_tensor = [self.output[name] for name in self.output]
+        feed_dict = {self.input[name]:feed_input[name] for name in self.input}
         print out_name, '=',
-        res = sess.run(out_tensor, feed_dict={self.input[name]:feed_input[name] for name in self.input})
+        res = sess.run(out_tensor, feed_dict=feed_dict)
         print res
+
+    def save(self, sess):
+        self.saver.save(sess, 'model.ckpt')
---
+++ train.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+import numpy as np
+import re
+from model import seq2seq_model
+# word embedding
+
+file = open('vector.txt', 'r')
+file.readline()
+voc = []
+embed = []
+k = 0
+while file:
+    line = file.readline().decode('utf-8')
+    k += 1
+    if k % 1000 == 0:
+        print k
+    split = re.split(' ', line)
+    voc.append(split[0])
+    vec = split[1:101]
+    for i in range(len(vec)):
+        vec[i] = eval(vec[i])
+    embed.append(vec)
+
+model = seq2seq_model(voc, embed, 100, 4)
+batch_size = 20
+maxi_length = 40
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+sess.run(tf.tables_initializer())
+"""
+try:
+    saver = tf.train.Saver()
+    saver.restore(sess, "model.ckpt")
+    print 'load finished'
+except:
+    sess.run(tf.global_variables_initializer())
+"""
+try:
+    for epoch in range(10):
+        file_post = open('weibo_pair_train_Q.post', 'r')
+        file_resp = open('weibo_pair_train_Q.response', 'r')
+        feed_dict = {'post':[], 'post_len':[], 'resp':[], 'resp_len':[]}
+        while file_post:
+            for _ in range(batch_size):
+                str_post = file_post.readline().decode('utf-8')
+                str_resp = file_resp.readline().decode('utf-8')
+                post = re.split(' ', str_post)
+                resp = re.split(' ', str_resp)
+                post[-1] = post[-1][:-1]
+                resp[-1] = resp[-1][:-1]
+                post = post + ['</s>']
+                resp = resp + ['</s>']
+                feed_dict['post'].append(post + [' '] * (maxi_length - len(post)))
+                feed_dict['resp'].append(resp + [' '] * (maxi_length - len(resp)))
+                feed_dict['post_len'].append(len(post))
+                feed_dict['resp_len'].append(len(resp))
+            model.step(sess, feed_dict)
+except KeyboardInterrupt:
+    model.save(sess)
